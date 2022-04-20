38dde87a5e43d959cfc5947073ef04d9d482f969
Sanja <sanja@Skynet.be>
2022-04-20 11:24:09 +0200

    first commit

---
+++ exercice_2.py
@@ -0,0 +1,10 @@
+L = int(input("Veuillez saisir le nombre de lignes : "))
+C =  int(input("Veuillez saisir le nombre de collones : "))
+for i in range(1,L+1):
+    for j in range(1,C+1):
+        if i == 1 or i == L or j == 1 or j == C:
+            print("* ",end="")
+        else:
+            print(" ", end=" ")
+
+    print()
